56ebebbecec9629edb7c76c281b9288d60449782
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-08-20 20:43:33 +0700

    Update ginkgo.sh

--- ginkgo.sh
+++ ginkgo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Removals
+rm -rf .repo/local_manifests
+
+# Sync the repositories
+/opt/crave/resync.sh
+
 git clone https://github.com/Miracleprjkt/device_xiaomi_ginkgo -b PixelOS device/xiaomi/ginkgo
 
 git clone https://github.com/Miracleprjkt/Vendor_Xiaomi_Ginkgo -b fourteen vendor/xiaomi/ginkgo
@@ -7,6 +13,9 @@ git clone https://github.com/Miracleprjkt/Vendor_Xiaomi_Ginkgo -b fourteen vendo
 git clone https://github.com/LineageOS/android_hardware_qcom-caf_common -b lineage-21.0 hardware/qcom-caf/common
 
 git clone https://github.com/tejas101k/kernel_xiaomi_ginkgo kernel/xiaomi/ginkgo
+
+export BUILD_USERNAME=Miraclev1
+export BUILD_HOSTNAME=crave
 
 ---------------------------------------------------------------------------------------------------------------------------
 . build/envsetup.sh
