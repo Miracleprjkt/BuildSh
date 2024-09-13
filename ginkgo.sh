3ec7397a37766ffc1d0d5829b2b704416c8ead23
FATTT🤖 <127039117+Miracleprjkt@users.noreply.github.com>
2024-09-13 06:27:24 -0700

    Update ginkgo.sh

--- ginkgo.sh
+++ ginkgo.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/Miracleprjkt/device_xiaomi_ginkgo -b PixelOS device/xiaomi/ginkgo
+git clone https://github.com/Miracleprjkt/device_ginkgo -b TenX-OS device/xiaomi/ginkgo
 
 git clone https://github.com/Miracleprjkt/Vendor_Xiaomi_Ginkgo -b fourteen vendor/xiaomi/ginkgo
 
-git clone https://github.com/Miracleprjkt/android_vendor_xiaomi_miuicamera vendor/xiaomi/miuicamera
+git clone https://github.com/mipa-ginkgo/android_vendor_miuicamera vendor/miuicamera
 
 git clone https://github.com/LineageOS/android_hardware_qcom-caf_common -b lineage-21.0 hardware/qcom-caf/common
 
-git clone https://github.com/tejas101k/kernel_xiaomi_ginkgo kernel/xiaomi/ginkgo
+git clone https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo -b fourteen/noksu kernel/xiaomi/ginkgo
+
+git clone https://gitlab.com/kutemeikito/rastamod69-clang -b clang-20.0 prebuilts/clang/host/linux-x86/clang-rastamod
+
+git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 14.0-caf-sm8150 hardware/qcom-caf/sm8150/display
 
 export BUILD_USERNAME=Miraclev1
 export BUILD_HOSTNAME=crave
@@ -16,8 +20,6 @@ export BUILD_HOSTNAME=crave
 ---------------------------------------------------------------------------------------------------------------------------
 . build/envsetup.sh
 
-lunch aosp_ginkgo-ap2a-userdebug
-
-mka bacon -j6
+brunch ginkgo
 
 -----------------------------------------------------------------------------------------------------------------------------
