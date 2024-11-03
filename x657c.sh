bcf1146f0d86493bd8dbd5070d8ca6007f64067e
FATTT🤖 <127039117+Miracleprjkt@users.noreply.github.com>
2024-11-03 19:25:17 +0700

    Update and rename ginkgo.sh to x657c.sh

---
+++ x657c.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf .repo/local_manifests/
+repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
+/opt/crave/resync.sh
+
+git clone https://github.com/Miracleprjkt/device_infinix_X657C -b los-18.1 device/infinix/X657C
+git clone https://github.com/Infinix-MT6761D-Development/vendor_infinix_X657C vendor/infinix/X657C
+
+export BUILD_USERNAME=Miracleprjkt
+export BUILD_HOSTNAME=crave
+
+source build/envsetup.sh
+brunch X657C
