83eea7c97073fa955bfbbaee74e3d80238686ef4
FATTT🤖 <127039117+Miracleprjkt@users.noreply.github.com>
2025-06-15 13:13:40 +0700

    Update ginkgo.sh

--- ginkgo.sh
+++ ginkgo.sh
@@ -5,30 +5,20 @@ rm -rf device/xiaomi
 rm -rf kernel/xiaomi
 rm -rf vendor/xiaomi
 
-repo init --depth=1 --no-repo-verify -u https://github.com/Los-Ext/manifest.git -b 15 --git-lfs -g default,-mips,-darwin,-notdefault
+repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs
 
 /opt/crave/resync.sh
 
-git clone https://github.com/Miracleprjkt/device_xiaomi_ginkgo -b LinegExt-22.2 device/xiaomi/ginkgo
+git clone https://github.com/Miracleprjkt/android_device_xiaomi_ginkgo device/xiaomi/ginkgo
 
-git clone https://github.com/kutemeikito/vendor_xiaomi_ginkgo -b fourteen vendor/xiaomi/ginkgo
+git clone https://github.com/Miracleprjkt/android_device_xiaomi_sm6125-common device/xiaomi/sm6125-common
 
-git clone https://github.com/Miracleprjkt/Signing_Keys vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sm6125 kernel/xiaomi/sm6125
 
-git clone https://github.com/mipa-ginkgo/android_vendor_miuicamera vendor/miuicamera
+git clone https://github.com/Skyblueborb/proprietary_vendor_xiaomi_ginkgo vendor/xiaomi/ginkgo
 
-git clone https://github.com/LineageOS/android_hardware_qcom-caf_common -b lineage-21.0 hardware/qcom-caf/common
-
-git clone https://github.com/kutemeikito/android_kernel_xiaomi_ginkgo -b fourteen/main kernel/xiaomi/ginkgo
-
-git clone https://gitlab.com/kutemeikito/rastamod69-clang -b clang-20.0 prebuilts/clang/host/linux-x86/clang-rastamod
-
-rm -rf hardware/qcom-caf/sm8150/display
-
-git clone https://github.com/crdroidandroid/android_hardware_qcom_display -b 14.0-caf-sm8150 hardware/qcom-caf/sm8150/display
+git clone https://github.com/Skyblueborb/proprietary_vendor_xiaomi_sm6125-common vendor/xiaomi/sm6125-common
 
 . build/envsetup.sh
-
-lunch lineage_ginkgo-userdebug
-
-mka bacon
+ 
+brunch ginkgo
