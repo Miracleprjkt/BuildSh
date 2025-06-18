db74b27b68d3fe019c7bfaae8ad18145c9e3c46d
FATTT🤖 <127039117+Miracleprjkt@users.noreply.github.com>
2025-06-18 20:10:36 +0700

    Update ginkgo.sh

--- ginkgo.sh
+++ ginkgo.sh
@@ -4,10 +4,13 @@ rm -rf .repo/local_manifests/
 rm -rf device/xiaomi
 rm -rf kernel/xiaomi
 rm -rf vendor/xiaomi
+rm -rf vendor/voltage
 
 repo init -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 --git-lfs
 
 /opt/crave/resync.sh
+
+git clone https://github.com/Miracleprjkt/vendor_voltage vendor/voltage
 
 git clone https://github.com/Miracleprjkt/android_device_xiaomi_ginkgo device/xiaomi/ginkgo
 
